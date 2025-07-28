e9e1093f3e1cbbdb6283a1945b83d6edce940746
liuaifei <liuaifei@ruyi.cn>
2025-07-28 16:35:54 +0800

    自定义主题色 & 菜单选项

---
+++ project_gable/Gable/GUI/Models/EThemeType.cs
@@ -0,0 +1,14 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Gable.GUI.Models
+{
+    public enum EThemeType
+    {
+        Auto = 0,
+        Light = 1,
+        Dark = 2,
+    }
+}
--- project_gable/Gable/GUI/ViewModels/MainWindowViewModel.cs
+++ project_gable/Gable/GUI/ViewModels/MainWindowViewModel.cs
@@ -1,5 +1,7 @@
 ﻿using System.Collections.ObjectModel;
 using System.Diagnostics;
+using Avalonia;
+using Avalonia.Styling;
 using CommunityToolkit.Mvvm.Input;
 using Gable.GUI.Models;
 
@@ -26,6 +28,81 @@ public partial class MainWindowViewModel : ViewModelBase
         Nodes.Add(root);
         root.addNode(new Node("Folder 1"));
     }
+
+    #region  Menu Commands
+    [RelayCommand]
+    private void Menu_NewFile() { /* 实现新建文件逻辑 */
+    }
+
+    [RelayCommand]
+    private void Menu_NewFolder() { /* 实现新建文件夹逻辑 */
+    }
+
+    [RelayCommand]
+    private void Menu_OpenProject() { /* 实现打开项目逻辑 */
+    }
+
+    [RelayCommand]
+    private void Menu_Setting() { /* 实现打开设置逻辑 */
+    }
+
+    [RelayCommand]
+    private void Menu_Exit() { /* 实现退出应用逻辑 */
+    }
+
+    [RelayCommand]
+    private void Menu_CompileSetting() { /* 实现编译设置逻辑 */
+    }
+
+    [RelayCommand]
+    private void Menu_QuickCompile() { /* 实现快速编译逻辑 */
+    }
+
+    [RelayCommand]
+    private void Menu_ImportExcel() { /* 实现导入Excel逻辑 */
+    }
+
+    [RelayCommand]
+    private void Menu_About() { /* 实现显示关于逻辑 */
+    }
+
+    [RelayCommand]
+    private void Menu_ThemeFollowSystem()
+    {
+        OnSetTheme(EThemeType.Auto);
+    }
+
+    [RelayCommand]
+    private void Menu_ThemeLight()
+    {
+        OnSetTheme(EThemeType.Light);
+    }
+
+    [RelayCommand]
+    private void Menu_ThemeDark()
+    {
+        OnSetTheme(EThemeType.Dark);
+    }
+
+    private void OnSetTheme(EThemeType type)
+    {
+        if (Application.Current is not null)
+        {
+            switch (type)
+            {
+                case EThemeType.Auto:
+                    Application.Current.RequestedThemeVariant = ThemeVariant.Default;
+                    break;
+                case EThemeType.Light:
+                    Application.Current.RequestedThemeVariant = ThemeVariant.Light;
+                    break;
+                case EThemeType.Dark:
+                    Application.Current.RequestedThemeVariant = ThemeVariant.Dark;
+                    break;
+            }
+        }
+    }
+    #endregion
 
     [RelayCommand]
     private void AddFolderNode()

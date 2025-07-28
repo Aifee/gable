39b30cc23a44dab20f2632bb3b62e3fb6f30f0e1
liuaifei <liuaifei@ruyi.cn>
2025-07-28 11:15:58 +0800

    程序入口整理

--- project_gable/Gable/Program.cs
+++ project_gable/Gable/Program.cs
@@ -1,21 +1,42 @@
-﻿using Avalonia;
-using System;
+﻿using System;
+using System.Runtime.InteropServices;
+using Avalonia;
+using Gable.CLI;
+using Gable.GUI;
 
 namespace Gable;
 
 sealed class Program
 {
+    // 用于分配控制台
+    [DllImport("kernel32.dll")]
+    private static extern bool AllocConsole();
+
     // Initialization code. Don't use any Avalonia, third-party APIs or any
     // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
     // yet and stuff might break.
     [STAThread]
-    public static void Main(string[] args) => BuildAvaloniaApp()
-        .StartWithClassicDesktopLifetime(args);
+    public static void Main(string[] args)
+    {
+        // 如果有参数，分配控制台
+        if (args.Length > 0)
+        {
+            AllocConsole();
+        }
+        Console.WriteLine($"Gable Start:{string.Join(" ", args)}");
+        Init();
+        if (args.Length > 0)
+        {
+            CLIMain.Start(args);
+        }
+        else
+        {
+            GUIMain.Start(args);
+        }
+    }
 
-    // Avalonia configuration, don't remove; also used by visual designer.
-    public static AppBuilder BuildAvaloniaApp()
-        => AppBuilder.Configure<App>()
-            .UsePlatformDetect()
-            .WithInterFont()
-            .LogToTrace();
+    private static void Init()
+    {
+        // ModelManager.Instance.Init();
+    }
 }

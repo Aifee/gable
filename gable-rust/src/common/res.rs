0eb49227f7c01c4b893509531069199dc3f2db55
liuaifei <liuaifei@ruyi.cn>
2025-08-04 14:48:47 +0800

    整理警告问题

--- gable-rust/src/common/res.rs
+++ gable-rust/src/common/res.rs
@@ -4,30 +4,30 @@ use eframe::egui;
 pub const FONT_ASSETS: &[u8] =
     include_bytes!(r"../../assets/fonts/NotoSansSC-VariableFont_wght.ttf");
 
-pub const ICON_DEFUALT: &[u8] = include_bytes!(r"../../assets/icons/error.png");
+// pub const ICON_DEFUALT: &[u8] = include_bytes!(r"../../assets/icons/error.png");
 
-pub const ICON_SETTING: &[u8] = include_bytes!("../../assets/icons/setting.png");
-pub const ICON_SEARCH: &[u8] = include_bytes!("../../assets/icons/search.png");
-pub const ICON_SYSTEM: &[u8] = include_bytes!("../../assets/icons/system.png");
+// pub const ICON_SETTING: &[u8] = include_bytes!("../../assets/icons/setting.png");
+// pub const ICON_SEARCH: &[u8] = include_bytes!("../../assets/icons/search.png");
+// pub const ICON_SYSTEM: &[u8] = include_bytes!("../../assets/icons/system.png");
 
-pub const ICON_CLIENT: &[u8] = include_bytes!("../../assets/icons/client.png");
-pub const ICON_SERVER: &[u8] = include_bytes!("../../assets/icons/server.png");
+// pub const ICON_CLIENT: &[u8] = include_bytes!("../../assets/icons/client.png");
+// pub const ICON_SERVER: &[u8] = include_bytes!("../../assets/icons/server.png");
 
 pub const ICON_FOLDER_BLUE: &[u8] = include_bytes!("../../assets/icons/folder_blue.png");
-pub const ICON_FOLDER_NORMAL: &[u8] = include_bytes!("../../assets/icons/folder_normal.png");
+// pub const ICON_FOLDER_NORMAL: &[u8] = include_bytes!("../../assets/icons/folder_normal.png");
 pub const ICON_EXCEL: &[u8] = include_bytes!("../../assets/icons/excel.png");
 pub const ICON_SHEET: &[u8] = include_bytes!("../../assets/icons/sheet.png");
 
-pub const ICON_C: &[u8] = include_bytes!("../../assets/icons/c.png");
-pub const ICON_CANGJIE: &[u8] = include_bytes!("../../assets/icons/cangjie.png");
-pub const ICON_CSHARP: &[u8] = include_bytes!("../../assets/icons/csharp.png");
-pub const ICON_GOLANG: &[u8] = include_bytes!("../../assets/icons/golang.png");
-pub const ICON_JAVA: &[u8] = include_bytes!("../../assets/icons/java.png");
-pub const ICON_JAVASCRIPT: &[u8] = include_bytes!("../../assets/icons/javascript.png");
-pub const ICON_LUA: &[u8] = include_bytes!("../../assets/icons/lua.png");
-pub const ICON_PYTHON: &[u8] = include_bytes!("../../assets/icons/python.png");
-pub const ICON_TYPESCRIPT: &[u8] = include_bytes!("../../assets/icons/typescript.png");
-pub const ICON_YAML: &[u8] = include_bytes!("../../assets/icons/yaml.png");
+// pub const ICON_C: &[u8] = include_bytes!("../../assets/icons/c.png");
+// pub const ICON_CANGJIE: &[u8] = include_bytes!("../../assets/icons/cangjie.png");
+// pub const ICON_CSHARP: &[u8] = include_bytes!("../../assets/icons/csharp.png");
+// pub const ICON_GOLANG: &[u8] = include_bytes!("../../assets/icons/golang.png");
+// pub const ICON_JAVA: &[u8] = include_bytes!("../../assets/icons/java.png");
+// pub const ICON_JAVASCRIPT: &[u8] = include_bytes!("../../assets/icons/javascript.png");
+// pub const ICON_LUA: &[u8] = include_bytes!("../../assets/icons/lua.png");
+// pub const ICON_PYTHON: &[u8] = include_bytes!("../../assets/icons/python.png");
+// pub const ICON_TYPESCRIPT: &[u8] = include_bytes!("../../assets/icons/typescript.png");
+// pub const ICON_YAML: &[u8] = include_bytes!("../../assets/icons/yaml.png");
 
 /// 加载图片
 // 修改 load_texture 支持从字节数据加载

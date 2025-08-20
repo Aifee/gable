caa436935a2f8e2969a8b965268a10f303f3ac44
liuaifei <liuaifei@ruyi.cn>
2025-08-20 10:23:11 +0800

    调整布局结构

--- gable-rust/src/gui/gable_log.rs
+++ gable-rust/src/gui/gable_log.rs
@@ -1,5 +1,5 @@
 use crate::gui::{component, datas::log::LogTrace};
-use eframe::egui::{Context, Frame, ScrollArea, TopBottomPanel};
+use eframe::egui::{Align, Context, Frame, Layout, ScrollArea, TopBottomPanel, Ui, Vec2};
 use egui_extras::{Column, TableBody, TableBuilder};
 
 pub(crate) struct GableLog {
@@ -24,95 +24,75 @@ impl GableLog {
             .default_height(300.0)
             .frame(Frame::side_top_panel(&ctx.style()).inner_margin(10.0))
             .show(ctx, |ui| {
-                ScrollArea::vertical().auto_shrink(true).show(ui, |ui| {
-                    TableBuilder::new(ui)
-                        .striped(true)
-                        .resizable(true)
-                        .column(Column::initial(self.time_col_width).resizable(true)) // 时间列
-                        .column(Column::initial(self.level_col_width).resizable(true)) // 级别列
-                        .column(Column::initial(self.target_col_width).resizable(true)) // 目标列
-                        .column(Column::remainder().resizable(true)) // 消息列
-                        .header(self.row_height, |mut header| {
-                            header.col(|ui| {
-                                ui.centered_and_justified(|ui| {
-                                    ui.label("时间");
-                                });
-                            });
-                            header.col(|ui| {
-                                ui.centered_and_justified(|ui| {
-                                    ui.label("级别");
-                                });
-                            });
-                            header.col(|ui| {
-                                ui.centered_and_justified(|ui| {
-                                    ui.label("目标");
-                                });
-                            });
-                            header.col(|ui| {
-                                ui.centered_and_justified(|ui| {
-                                    ui.label("消息");
-                                });
-                            });
-                        })
-                        .body(|mut body: TableBody<'_>| {
-                            if let Some(log_records) = LogTrace::get_log_records() {
-                                if let Ok(records) = log_records.lock() {
-                                    let row_count: usize = records.len();
-                                    if row_count > 0 {
-                                        body.rows(self.row_height, row_count, |mut row| {
-                                            let row_index = row_count - row.index() - 1;
-                                            if let Some(record) = records.get(row_index) {
-                                                row.col(|ui| {
-                                                    component::log_text(
-                                                        ui,
-                                                        &record.timestamp,
-                                                        record.level,
-                                                    );
-                                                });
-                                                row.col(|ui| {
-                                                    component::log_text(
-                                                        ui,
-                                                        &format!("{:?}", record.level),
-                                                        record.level,
-                                                    );
-                                                });
-                                                row.col(|ui| {
-                                                    component::log_text(
-                                                        ui,
-                                                        &record.target,
-                                                        record.level,
-                                                    );
-                                                });
-                                                row.col(|ui| {
-                                                    component::log_text(
-                                                        ui,
-                                                        &record.args,
-                                                        record.level,
-                                                    );
-                                                });
-                                            }
+                self.ongui_table(ui);
+            });
+    }
+
+    fn ongui_table(&mut self, ui: &mut Ui) {
+        ScrollArea::vertical().auto_shrink(true).show(ui, |ui| {
+            TableBuilder::new(ui)
+                .striped(true)
+                .resizable(true)
+                .column(Column::initial(self.time_col_width).resizable(true)) // 时间列
+                .column(Column::initial(self.level_col_width).resizable(true)) // 级别列
+                .column(Column::initial(self.target_col_width).resizable(true)) // 目标列
+                .column(Column::remainder().resizable(true)) // 消息列
+                .header(self.row_height, |mut header| {
+                    header.col(|ui| {
+                        ui.centered_and_justified(|ui| {
+                            ui.label("时间");
+                        });
+                    });
+                    header.col(|ui| {
+                        ui.centered_and_justified(|ui| {
+                            ui.label("级别");
+                        });
+                    });
+                    header.col(|ui| {
+                        ui.centered_and_justified(|ui| {
+                            ui.label("目标");
+                        });
+                    });
+                    header.col(|ui| {
+                        ui.centered_and_justified(|ui| {
+                            ui.label("消息");
+                        });
+                    });
+                })
+                .body(|mut body: TableBody<'_>| {
+                    if let Some(log_records) = LogTrace::get_log_records() {
+                        if let Ok(records) = log_records.lock() {
+                            let row_count: usize = records.len();
+                            if row_count > 0 {
+                                body.rows(self.row_height, row_count, |mut row| {
+                                    let row_index = row_count - row.index() - 1;
+                                    if let Some(record) = records.get(row_index) {
+                                        row.col(|ui| {
+                                            component::log_text(
+                                                ui,
+                                                &record.timestamp,
+                                                record.level,
+                                            );
                                         });
-                                    } else {
-                                        body.row(self.row_height, |mut row| {
-                                            row.col(|ui| {
-                                                ui.label("暂无日志");
-                                            });
-                                            row.col(|ui| {
-                                                ui.label("");
-                                            });
-                                            row.col(|ui| {
-                                                ui.label("");
-                                            });
-                                            row.col(|ui| {
-                                                ui.label("");
-                                            });
+                                        row.col(|ui| {
+                                            component::log_text(
+                                                ui,
+                                                &format!("{:?}", record.level),
+                                                record.level,
+                                            );
+                                        });
+                                        row.col(|ui| {
+                                            component::log_text(ui, &record.target, record.level);
+                                        });
+                                        row.col(|ui| {
+                                            component::log_text(ui, &record.args, record.level);
                                         });
                                     }
-                                }
+                                });
                             } else {
                                 body.row(self.row_height, |mut row| {
                                     row.col(|ui| {
-                                        ui.label("日志系统未初始化");
+                                        ui.label("暂无日志");
                                     });
                                     row.col(|ui| {
                                         ui.label("");
@@ -125,8 +105,24 @@ impl GableLog {
                                     });
                                 });
                             }
+                        }
+                    } else {
+                        body.row(self.row_height, |mut row| {
+                            row.col(|ui| {
+                                ui.label("日志系统未初始化");
+                            });
+                            row.col(|ui| {
+                                ui.label("");
+                            });
+                            row.col(|ui| {
+                                ui.label("");
+                            });
+                            row.col(|ui| {
+                                ui.label("");
+                            });
                         });
+                    }
                 });
-            });
+        });
     }
 }

760e4023c292b06f4df6a2e62cc4627beabdfafa
liuaifei <liuaifei@ruyi.cn>
2025-09-01 17:44:55 +0800

    设置界面布局调整

--- gable-rust/src/gui/gable_build_setting.rs
+++ gable-rust/src/gui/gable_build_setting.rs
@@ -3,10 +3,10 @@ use crate::{
     gui::datas::edevelop_type::EDevelopType,
 };
 use eframe::egui::{
-    Align, Button, CentralPanel, Color32, ComboBox, Context, Frame, Image, Layout, RichText,
-    ScrollArea, Sense, Separator, SidePanel, TopBottomPanel, Ui, Vec2, Window,
+    Align, Align2, Button, CentralPanel, Color32, ComboBox, Context, FontId, Frame, Image, Label,
+    Layout, Rect, Response, RichText, ScrollArea, Sense, Separator, SidePanel, TextureHandle,
+    TopBottomPanel, Ui, Vec2, Window,
 };
-use serde::de::value;
 
 pub struct GableBuildSetting {
     pub visible: bool,
@@ -29,16 +29,11 @@ impl GableBuildSetting {
     }
 
     pub fn ongui(&mut self, ctx: &Context) {
-        let Self {
-            visible,
-            add_selected,
-            dev_list,
-            selected_index,
-        } = self;
-
-        if !*visible {
+        if !self.visible {
             return;
         }
+
+        let mut visible = self.visible;
 
         let window = Window::new("构建设置")
             .resizable(true)
@@ -46,76 +41,14 @@ impl GableBuildSetting {
             .default_width(960.0)
             .default_height(600.0)
             .vscroll(false)
-            .open(&mut self.visible);
+            .open(&mut visible);
         window.show(ctx, |ui| {
             SidePanel::left("left_panel")
                 .resizable(true)
                 .default_width(300.0)
                 .width_range(300.0..=700.0)
                 .show_inside(ui, |ui| {
-                    ui.heading("开发环境");
-                    let available_height = ui.available_height();
-                    let combo_area_height = 50.0;
-
-                    ScrollArea::vertical()
-                        .auto_shrink(false)
-                        .max_height(available_height - combo_area_height)
-                        .show(ui, |ui| {
-                            let button_width = ui.available_width() - 30.0;
-                            for (index, v) in dev_list.iter().enumerate() {
-                                let texture = res::load_develop_icon(ctx, v);
-                                let image = Image::new(&texture)
-                                    .tint(Color32::WHITE)
-                                    .fit_to_exact_size(Vec2::new(24.0, 24.0));
-                                let button_size: Vec2 = Vec2::new(button_width, 40.0);
-                                ui.horizontal(|ui| {
-                                    let tab_button = Button::new("")
-                                        .fill(if *selected_index == index {
-                                            utils::get_selected_color(ctx)
-                                        } else {
-                                            Color32::TRANSPARENT
-                                        })
-                                        .min_size(Vec2::new(button_size.x - 20.0, button_size.y));
-
-                                    let response = ui.add_sized(button_size, tab_button);
-                                    if response.clicked() {
-                                        *selected_index = index;
-                                    }
-
-                                    // 手动在按钮内绘制图标和文本
-                                    let rect = response.rect;
-                                    ui.put(rect, |ui: &mut Ui| {
-                                        ui.horizontal(|ui| {
-                                            ui.add_space(8.0); // 左边距
-                                            ui.add(image);
-                                            ui.add_space(8.0); // 图标和文本之间的空间
-                                            ui.label(v.to_string());
-                                        });
-                                        ui.allocate_rect(
-                                            ui.available_rect_before_wrap(),
-                                            Sense::hover(),
-                                        )
-                                    });
-                                });
-                            }
-                        });
-
-                    ui.separator();
-                    ui.vertical_centered(|ui| {
-                        ui.horizontal(|ui| {
-                            ComboBox::from_id_salt("develop_type")
-                                .selected_text(format!("{add_selected:?}"))
-                                .show_ui(ui, |ui| {
-                                    for item in EDevelopType::iter() {
-                                        ui.selectable_value(add_selected, *item, item.to_string());
-                                    }
-                                });
-                            ui.add_space(5.0);
-                            if ui.add_sized([120.0, 26.0], Button::new("添加")).clicked() {
-                                dev_list.push(add_selected.clone());
-                            }
-                        });
-                    });
+                    self.ongui_left_panel(ui);
                 });
 
             TopBottomPanel::bottom("bottom_panel")
@@ -140,6 +73,75 @@ impl GableBuildSetting {
                 ScrollArea::vertical().auto_shrink(false).show(ui, |ui| {
                     Self::lorem_ipsum(ui);
                 });
+            });
+        });
+        self.visible = visible;
+    }
+
+    fn ongui_left_panel(&mut self, ui: &mut Ui) {
+        ui.heading("开发环境");
+        let available_height = ui.available_height();
+        let combo_area_height = 50.0;
+
+        ScrollArea::vertical()
+            .auto_shrink(false)
+            .max_height(available_height - combo_area_height)
+            .show(ui, |ui| {
+                for (index, v) in self.dev_list.iter().enumerate() {
+                    let texture: TextureHandle = res::load_develop_icon(ui.ctx(), v);
+                    let image: Image<'_> = Image::new(&texture)
+                        .tint(Color32::WHITE)
+                        .fit_to_exact_size(Vec2::new(24.0, 24.0));
+                    let button_size: Vec2 = Vec2::new(ui.available_width(), 40.0);
+                    ui.horizontal(|ui| {
+                        let tab_button = Button::new("")
+                            .fill(if self.selected_index == index {
+                                utils::get_selected_color(ui.ctx())
+                            } else {
+                                Color32::TRANSPARENT
+                            })
+                            .min_size(Vec2::new(button_size.x - 20.0, button_size.y));
+
+                        let response: Response = ui.add_sized(button_size, tab_button);
+                        if response.clicked() {
+                            self.selected_index = index;
+                        }
+
+                        let rect: Rect = response.rect;
+                        ui.put(rect, |ui: &mut Ui| {
+                            ui.horizontal(|ui| {
+                                ui.add_space(8.0);
+                                ui.add(image);
+                                ui.add_space(8.0);
+                                ui.painter().text(
+                                    ui.available_rect_before_wrap().left_top()
+                                        + Vec2::new(0.0, 18.0),
+                                    Align2::LEFT_CENTER,
+                                    v.to_string(),
+                                    FontId::default(),
+                                    ui.style().visuals.text_color(),
+                                );
+                            });
+                            ui.allocate_rect(ui.available_rect_before_wrap(), Sense::hover())
+                        });
+                    });
+                }
+            });
+
+        ui.separator();
+        ui.vertical_centered(|ui| {
+            ui.horizontal(|ui| {
+                ComboBox::from_id_salt("develop_type")
+                    .selected_text(format!("{:?}", self.add_selected))
+                    .show_ui(ui, |ui| {
+                        for item in EDevelopType::iter() {
+                            ui.selectable_value(&mut self.add_selected, *item, item.to_string());
+                        }
+                    });
+                ui.add_space(5.0);
+                if ui.add_sized([120.0, 26.0], Button::new("添加")).clicked() {
+                    self.dev_list.push(self.add_selected.clone());
+                }
             });
         });
     }

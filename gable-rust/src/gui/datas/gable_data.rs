cd4630c8c32f54470c2612908eec03fca21735e9
liuaifei <liuaifei@ruyi.cn>
2025-09-04 16:34:04 +0800

    添加主键概念

--- gable-rust/src/gui/datas/gable_data.rs
+++ gable-rust/src/gui/datas/gable_data.rs
@@ -13,63 +13,64 @@ pub struct GableData {
 
 impl GableData {
     /// 获取普通表的有效的数据头
-    pub fn get_valid_normal_heads(&self, keyword: &str) -> BTreeMap<u16, BTreeMap<u32, &CellData>> {
+    pub fn get_valid_normal_heads(
+        &self,
+        keyword: &str,
+    ) -> (
+        BTreeMap<u16, BTreeMap<u32, &CellData>>,
+        BTreeMap<u16, BTreeMap<u32, &CellData>>,
+    ) {
+        // 主键表头
+        let mut valids_main: BTreeMap<u16, BTreeMap<u32, &CellData>> = BTreeMap::new();
+        // 除主键外的其他表头数据
         let mut valids: BTreeMap<u16, BTreeMap<u32, &CellData>> = BTreeMap::new();
 
         let max_col = self.max_col + 1;
         for col_index in 1..max_col {
             let field_celldata =
                 if let Some(row_data) = self.heads.get(&constant::TABLE_DATA_ROW_FIELD) {
-                    row_data.get(&col_index)
+                    row_data.get(&col_index).unwrap()
                 } else {
-                    None
+                    continue;
                 };
             let type_celldata =
                 if let Some(row_data) = self.heads.get(&constant::TABLE_DATA_ROW_TYPE) {
-                    row_data.get(&col_index)
+                    row_data.get(&col_index).unwrap()
                 } else {
-                    None
+                    continue;
                 };
             let keyword_celldata =
                 if let Some(row_data) = self.heads.get(&constant::TABLE_DATA_ROW_TARGET) {
-                    row_data.get(&col_index)
+                    row_data.get(&col_index).unwrap()
                 } else {
-                    None
+                    continue;
                 };
             // 验证字段是否合法
-            if let Some(field_celldata) = field_celldata {
-                if !field_celldata.verify_lawful() {
-                    continue;
-                }
-            } else {
+            if !field_celldata.verify_lawful() {
                 continue;
             }
 
             // 验证数据类型是否合法
-            if let Some(type_celldata) = type_celldata {
-                if !type_celldata.verify_lawful() {
-                    continue;
-                }
-            } else {
+            if !type_celldata.verify_lawful() {
                 continue;
             }
             // 验证keyword是否合法
-            if let Some(keyword_celldata) = keyword_celldata {
-                if !keyword_celldata.verify_lawful() {
-                    continue;
-                }
-                if !keyword_celldata.value.contains(keyword) {
-                    continue;
-                }
-            } else {
+            if !keyword_celldata.verify_lawful() {
+                continue;
+            }
+            if !keyword_celldata.value.contains(keyword) {
                 continue;
             }
 
             let mut col_datas: BTreeMap<u32, &CellData> = BTreeMap::new();
-            col_datas.insert(constant::TABLE_DATA_ROW_FIELD, field_celldata.unwrap());
-            col_datas.insert(constant::TABLE_DATA_ROW_TYPE, type_celldata.unwrap());
-            valids.insert(col_index, col_datas);
+            col_datas.insert(constant::TABLE_DATA_ROW_FIELD, field_celldata);
+            col_datas.insert(constant::TABLE_DATA_ROW_TYPE, type_celldata);
+            if field_celldata.value.contains("*") {
+                valids_main.insert(col_index, col_datas);
+            } else {
+                valids.insert(col_index, col_datas);
+            }
         }
-        return valids;
+        return (valids_main, valids);
     }
 }
--- gable-rust/src/gui/datas/tree_data.rs
+++ gable-rust/src/gui/datas/tree_data.rs
@@ -22,19 +22,44 @@ impl TreeData {
     }
 
     fn normal_json_data(&self, keyword: &str) -> Vec<Map<String, Value>> {
-        let valid_heads: BTreeMap<u16, BTreeMap<u32, &CellData>> =
-            self.content.get_valid_normal_heads(keyword);
+        let (valids_main, valids) = self.content.get_valid_normal_heads(keyword);
         let mut items: Vec<Map<String, Value>> = Vec::new();
-        let max_row = self.content.max_row + 1;
+        let max_row: u32 = self.content.max_row + 1;
         for row_index in constant::TABLE_DATA_ROW_TOTAL..=max_row {
-            let row_data = if let Some(row_data) = self.content.cells.get(&row_index) {
-                row_data
-            } else {
-                continue;
-            };
+            let row_data: &BTreeMap<u16, CellData> =
+                if let Some(row_data) = self.content.cells.get(&row_index) {
+                    row_data
+                } else {
+                    continue;
+                };
+            let mut row_valid: bool = true;
             let mut item_data: Map<String, Value> = Map::new();
-            for (col_index, head_data) in valid_heads.iter() {
-                let value_cell = if let Some(value_cell) = row_data.get(col_index) {
+            // 检测行数据是否有效，主键没有数据，行数据无效则跳过
+            for (col_index, head_data) in valids_main.iter() {
+                let value_cell: &CellData = if let Some(value_cell) = row_data.get(col_index) {
+                    value_cell
+                } else {
+                    row_valid = false;
+                    continue;
+                };
+                if value_cell.value.is_empty() {
+                    row_valid = false;
+                    continue;
+                };
+                let type_cell: &&CellData = head_data.get(&constant::TABLE_DATA_ROW_TYPE).unwrap();
+                let field_cell: &&CellData =
+                    head_data.get(&constant::TABLE_DATA_ROW_FIELD).unwrap();
+                let value: Value = Self::get_json_value(type_cell, value_cell);
+                let field_value: String = field_cell.value.replace("*", "");
+                item_data.insert(field_value, value);
+            }
+            // 行数据无效
+            if !row_valid {
+                continue;
+            }
+
+            for (col_index, head_data) in valids.iter() {
+                let value_cell: &CellData = if let Some(value_cell) = row_data.get(col_index) {
                     value_cell
                 } else {
                     continue;
@@ -42,104 +67,39 @@ impl TreeData {
                 if value_cell.value.is_empty() {
                     continue;
                 };
-                let type_cell = head_data.get(&constant::TABLE_DATA_ROW_TYPE).unwrap();
-                let data_type = EDataType::convert(&type_cell.value);
-                let field_cell = head_data.get(&constant::TABLE_DATA_ROW_FIELD).unwrap();
-                match data_type {
-                    EDataType::Int | EDataType::Time | EDataType::Date | EDataType::Enum => {
-                        item_data.insert(
-                            field_cell.value.clone(),
-                            Value::from(value_cell.parse_int()),
-                        );
-                    }
-                    EDataType::Boolean => {
-                        item_data.insert(
-                            field_cell.value.clone(),
-                            Value::from(value_cell.parse_bool()),
-                        );
-                    }
-                    EDataType::Float => {
-                        item_data.insert(
-                            field_cell.value.clone(),
-                            Value::from(value_cell.parse_float()),
-                        );
-                    }
-                    EDataType::Vector2 => {
-                        item_data.insert(
-                            field_cell.value.clone(),
-                            Value::from(value_cell.to_json_vector2()),
-                        );
-                    }
-                    EDataType::Vector3 => {
-                        item_data.insert(
-                            field_cell.value.clone(),
-                            Value::from(value_cell.to_json_vector3()),
-                        );
-                    }
-                    EDataType::Vector4 => {
-                        item_data.insert(
-                            field_cell.value.clone(),
-                            Value::from(value_cell.to_json_vector4()),
-                        );
-                    }
-                    EDataType::IntArr => {
-                        item_data.insert(
-                            field_cell.value.clone(),
-                            Value::from(value_cell.to_json_int_array()),
-                        );
-                    }
-                    EDataType::StringArr => {
-                        item_data.insert(
-                            field_cell.value.clone(),
-                            Value::from(value_cell.to_json_string_array()),
-                        );
-                    }
-                    EDataType::BooleanArr => {
-                        item_data.insert(
-                            field_cell.value.clone(),
-                            Value::from(value_cell.to_json_bool_array()),
-                        );
-                    }
-                    EDataType::FloatArr => {
-                        item_data.insert(
-                            field_cell.value.clone(),
-                            Value::from(value_cell.to_json_float_array()),
-                        );
-                    }
-                    EDataType::Vector2Arr => {
-                        item_data.insert(
-                            field_cell.value.clone(),
-                            Value::from(value_cell.to_json_vector2_array()),
-                        );
-                    }
-                    EDataType::Vector3Arr => {
-                        item_data.insert(
-                            field_cell.value.clone(),
-                            Value::from(value_cell.to_json_vector3_array()),
-                        );
-                    }
-                    EDataType::Vector4Arr => {
-                        item_data.insert(
-                            field_cell.value.clone(),
-                            Value::from(value_cell.to_json_vector4_array()),
-                        );
-                    }
-                    EDataType::Percentage | EDataType::Permillage | EDataType::Permian => {
-                        item_data.insert(
-                            field_cell.value.clone(),
-                            Value::from(value_cell.parse_float()),
-                        );
-                    }
-                    _ => {
-                        item_data.insert(
-                            field_cell.value.clone(),
-                            Value::from(value_cell.value.clone()),
-                        );
-                    }
-                }
+                let type_cell: &&CellData = head_data.get(&constant::TABLE_DATA_ROW_TYPE).unwrap();
+                let field_cell: &&CellData =
+                    head_data.get(&constant::TABLE_DATA_ROW_FIELD).unwrap();
+                let value: Value = Self::get_json_value(type_cell, value_cell);
+                item_data.insert(field_cell.value.clone(), value);
             }
             items.push(item_data);
         }
         return items;
+    }
+
+    fn get_json_value(type_cell: &CellData, value_cell: &CellData) -> Value {
+        let data_type: EDataType = EDataType::convert(&type_cell.value);
+        match data_type {
+            EDataType::Int | EDataType::Time | EDataType::Date | EDataType::Enum => {
+                Value::from(value_cell.parse_int())
+            }
+            EDataType::Boolean => Value::from(value_cell.parse_bool()),
+            EDataType::Float => Value::from(value_cell.parse_float()),
+            EDataType::Vector2 => Value::from(value_cell.to_json_vector2()),
+            EDataType::Vector3 => Value::from(value_cell.to_json_vector3()),
+            EDataType::Vector4 => Value::from(value_cell.to_json_vector4()),
+            EDataType::IntArr => Value::from(value_cell.to_json_int_array()),
+            EDataType::StringArr => Value::from(value_cell.to_json_string_array()),
+            EDataType::BooleanArr => Value::from(value_cell.to_json_bool_array()),
+            EDataType::FloatArr => Value::from(value_cell.to_json_float_array()),
+            EDataType::Vector2Arr => Value::from(value_cell.to_json_vector2_array()),
+            EDataType::Vector3Arr => Value::from(value_cell.to_json_vector3_array()),
+            EDataType::Vector4Arr => Value::from(value_cell.to_json_vector4_array()),
+            EDataType::Percentage | EDataType::Permillage | EDataType::Permian => {
+                Value::from(value_cell.parse_float())
+            }
+            _ => Value::from(value_cell.value.clone()),
+        }
     }
 }

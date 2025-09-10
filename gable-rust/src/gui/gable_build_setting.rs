2bc036257e144e52ac03be1e0d893fc389182d7c
liuaifei <liuaifei@ruyi.cn>
2025-09-10 14:05:00 +0800

    设置界面默认不打开

--- gable-rust/src/gui/gable_build_setting.rs
+++ gable-rust/src/gui/gable_build_setting.rs
@@ -24,7 +24,7 @@ pub struct GableBuildSetting {
 impl GableBuildSetting {
     pub fn new() -> Self {
         Self {
-            visible: true,
+            visible: false,
             add_selected: EDevelopType::Cpp,
             selected_index: 0,
         }
